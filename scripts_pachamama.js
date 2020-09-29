26345985a583f3fb691cf1e78e11887db4cdb0e6
jenyferlima <38404317+jenyferlima@users.noreply.github.com>
2020-09-29 16:37:55 -0300

    Update scripts_pachamama.js

--- scripts_pachamama.js
+++ scripts_pachamama.js
@@ -1,5 +1,3 @@
-<script>
-	$(document).ready(() => {
-		$('.vitrine-6058957').before('<img src="https://cdn.awsli.com.br/1641/1641875/arquivos/graf_cabelo.svg" class="grafismo_titulo">>')
-	})
-</script>
+$(document).ready(() => {
+	$('.vitrine-6058957').before('<img src="https://cdn.awsli.com.br/1641/1641875/arquivos/graf_cabelo.svg" class="grafismo_titulo">>')
+})
