76aeccdcb45b896edb606da4138faff7e213a962
jenyferlima <38404317+jenyferlima@users.noreply.github.com>
2020-09-29 16:43:33 -0300

    Update scripts_pachamama.js

--- scripts_pachamama.js
+++ scripts_pachamama.js
@@ -1,3 +1,3 @@
 $(document).ready(() => {
-	$('.vitrine-6058957').before('<img src="https://cdn.awsli.com.br/1641/1641875/arquivos/graf_cabelo.svg" class="grafismo_titulo">>')
+	$('.vitrine-6058957').before('<img src="https://cdn.awsli.com.br/1641/1641875/arquivos/297-589.png" class="banner_meio">')
 })
